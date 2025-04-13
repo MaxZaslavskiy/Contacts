e2a0314f49ac08863acccd5639a2f04bad387c81
Lenovo <Lenovo@WIN-ORN5S7MNB9U>
2025-04-13 17:27:38 +0300

    Добавьте файлы проекта.

---
+++ testAgain/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using testAgain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testAgain.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()  // contacts
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Feedback feedback) 
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ testAgain/Models/Feedback.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testAgain.Models
+{
+    public class Feedback
+    {
+        [Display(Name = "Введіть ім'я")]
+        [Required(ErrorMessage = "U need to type ur name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Введіть фамілію")]
+        [Required(ErrorMessage = "U need to type ur surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Введіть вік")]
+        [Required(ErrorMessage = "U need to type ur name")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введіть емаїл")]
+        [Required(ErrorMessage = "U need to type ur email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введіть повідомлення")]
+        [Required(ErrorMessage = "U need to type ur message")]
+        [StringLength(10, ErrorMessage = "need more than 10 words")]
+        public string Message { get; set; }
+
+    }
+}
